a8abf4d7528ed4cbaf1d54ea1942a88c62feaabb
GX2H912 <GX2H912@gmail.com>
2016-03-25 19:01:58 -0400

    Create Command_pusheen.java

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_pusheen.java
@@ -0,0 +1,49 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+@CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "The one and only Pusheen", usage = "/<command>")
+public class Command_pusheen extends FreedomCommand
+{
+    
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        String name = sender.getName();
+        
+        if (args.length > 1)
+        {
+            msg(ChatColor.RED + "Incorrect args!");
+            return false;
+        }
+        
+        switch(name)
+        {
+            case "Enderbreeze672":
+                FUtil.adminAction(name, "- Summoning PUSHEEN", true);
+                for (Player player : server.getOnlinePlayers())
+                {
+                    ItemStack pusheen = new ItemStack(Material.CAKE, 1);
+                    ItemMeta meta = pusheen.getItemMeta();
+                    meta.setDisplayName(ChatColor.BLUE + "Pusheen");
+                    pusheen.setItemMeta(meta);
+                    player.getInventory().addItem(pusheen);
+                }
+            break;
+        default:
+                msg(ChatColor.RED + "Well, anyways Enderbreeze672 owns this command aka FireCrystal12's sister");
+                break;
+        }
+        return true;
+    }
+}
